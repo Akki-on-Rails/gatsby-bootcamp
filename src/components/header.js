b69d85aa1a440abd832a7890d87d3af5b8472444
Frank Gerngross <f.gerngross@googlemail.com>
2022-12-09 08:14:49 +0100

    created css module for the header.

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import * as headerStyles from './header.module.scss'
 
 const Header = () => {
   return (
@@ -7,7 +8,7 @@ const Header = () => {
       <h1>Frank Gerngroß</h1>
       <nav>
         <ul>
-          <li><Link to='/'>Home</Link></li>
+          <li><Link className={headerStyles.link} to='/'>Home</Link></li>
           <li><Link to='/about'>About</Link></li>
           <li><Link to='/blog'>Blog</Link></li>
           <li><Link to='/contact'>Contact</Link></li>
