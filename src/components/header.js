76db7cd351f42eae87886eaa2d04983bbc6248d8
Frank Gerngross <f.gerngross@googlemail.com>
2022-12-09 08:48:49 +0100

    added css module for layout and added some header styles

--- src/components/header.js
+++ src/components/header.js
@@ -4,14 +4,15 @@ import * as headerStyles from './header.module.scss'
 
 const Header = () => {
   return (
-    <header>
-      <h1>Frank Gerngroß</h1>
+    <header classNmae={headerStyles.header}>
+      <h1>
+        <Link className={headerStyles.title} to="/">Frank Gerngroß</Link></h1>
       <nav>
-        <ul>
-          <li><Link className={headerStyles.link} to='/'>Home</Link></li>
-          <li><Link to='/about'>About</Link></li>
-          <li><Link to='/blog'>Blog</Link></li>
-          <li><Link to='/contact'>Contact</Link></li>
+        <ul className={headerStyles.navList}>
+          <li><Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to='/'>Home</Link></li>
+          <li><Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to='/about'>About</Link></li>
+          <li><Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to='/blog'>Blog</Link></li>
+          <li><Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to='/contact'>Contact</Link></li>
         </ul>
       </nav>
     </header>
