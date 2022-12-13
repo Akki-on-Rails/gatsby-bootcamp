f1f8ae14b4a3d5e0420ec086253c6f4a58282fea
Frank Gerngross <f.gerngross@googlemail.com>
2022-12-13 12:00:52 +0100

    prepared a template for the blog posts

---
+++ src/templates/blog.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Layout from '../components/layout'
+
+const Blog = () => {
+  return (
+    <Layout>
+      This is the Template for our Blog posts
+    </Layout>
+  )
+}
+
+export default Blog
