22690018e7791cd8bd565a228bc568a76dd57d30
Frank Gerngross <f.gerngross@googlemail.com>
2022-12-13 11:02:28 +0100

    fetched data from markdown files for the blog list

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,15 +2,15 @@ import React from 'react';
 import { graphql, useStaticQuery } from 'gatsby';
 
 const Footer = () => {
-const data = useStaticQuery(graphql`
-  query {
-    site {
-      siteMetadata {
-        author
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          author
+        }
       }
     }
-  }
-`)
+  `)
 
   return (
     <footer>
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,12 +1,34 @@
 import React from 'react';
+import { graphql, useStaticQuery } from 'gatsby';
 
 import Layout from '../components/layout';
 
 const BlogPage = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              title
+              date
+            }
+            html
+            excerpt
+          }
+        }
+      }
+    }
+  `)
+
   return (
     <Layout>
       <h1>Blog</h1>
-      <h2>Posts will show up here later</h2>
+      <ol>
+        {data.allMarkdownRemark.edges.map(data => (
+          <li><h2>{data.node.frontmatter.title}</h2><p>{data.node.frontmatter.date}</p></li>
+        ))}
+      </ol>
     </Layout>
   )
 }
