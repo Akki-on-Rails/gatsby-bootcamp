7930cc3e42b66f6eb7e3e28d78370ed002584439
Frank Gerngross <f.gerngross@googlemail.com>
2022-12-15 13:48:43 +0100

    added style for blog page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link, graphql, useStaticQuery } from 'gatsby';
 
 import Layout from '../components/layout';
+import * as blogStyles from './blog.module.scss';
 
 const BlogPage = () => {
   const data = useStaticQuery(graphql`
@@ -25,9 +26,9 @@ const BlogPage = () => {
   return (
     <Layout>
       <h1>Blog</h1>
-      <ol>
+      <ol className={blogStyles.posts}>
         {[...data.allMarkdownRemark.edges].reverse().map(data => (
-          <li>
+          <li className={blogStyles.post}>
             <Link to={`/blog/${data.node.fields.slug}`}>
               <h2>{data.node.frontmatter.title}</h2>
               <p>{data.node.frontmatter.date}</p>
